c3546b125c70a74bd1808cbf3db912e5a24d81c9
BouajilaHamza <hamza.bouajila@enetcom.u-sfax.tn>
2024-12-01 23:57:44 +0100

    add setup.sh , fix snowflake load data,add notebooks, comment bandit in pre-commit hooks config file

--- app/storage/snowflake.py
+++ app/storage/snowflake.py
@@ -1,6 +1,8 @@
+import pandas as pd
 import snowflake.connector
 
 from app.setup.config import settings
+from app.setup.logging_config import setup_logging
 
 
 class SnowflakeLoader:
@@ -10,7 +12,7 @@ class SnowflakeLoader:
             password=settings.SNOWFLAKE_PASSWORD,
             account=settings.SNOWFLAKE_ACCOUNT,
         )
-
+        self.logger = setup_logging("SnowflakeLoader")
         self.cursor = self.conn.cursor()
 
     def clean_data(self, raw_data):
@@ -40,7 +42,7 @@ class NewsDataLoader(SnowflakeLoader):
         for record in cleaned_data:
             self.cursor.execute(
                 "INSERT INTO NewsData (field1, field2) VALUES (%s, %s)",
-                (record["field1"], record["field2"]),
+                (pd.DataFrame(record["field1"]), pd.DataFrame(record["field2"])),
             )
 
 
@@ -57,9 +59,29 @@ class BitcoinDataLoader(SnowflakeLoader):
         return [raw_data]
 
     def load_data(self, data):
-        cleaned_data = self.clean_data(data)
-        for record in cleaned_data:
-            self.cursor.execute(
-                "INSERT INTO BitcoinData (field1, field2) VALUES (%s, %s)",
-                (record["field1"], record["field2"]),
-            )
+        try:
+            cleaned_data = self.clean_data(data)
+            for record in cleaned_data:
+                self.cursor.execute("CREATE DATABASE IF NOT EXISTS NewsDataDatabase")
+                self.cursor.execute("USE DATABASE NewsDataDatabase")
+                self.cursor.execute(
+                    "CREATE TABLE IF NOT EXISTS NewsData1 (price VARCHAR(255), volume_24h VARCHAR(255), volume_change_24h VARCHAR(255), percent_change_1h VARCHAR(255), percent_change_24h VARCHAR(255), percent_change_7d VARCHAR(255), market_cap VARCHAR(255), market_cap_dominance VARCHAR(255), fully_diluted_market_cap VARCHAR(255), last_updated VARCHAR(255))"
+                )
+                for i in range(len(record["data"])):
+                    requette = """ INSERT INTO NewsData1 (price, volume_24h, volume_change_24h, percent_change_1h, percent_change_24h, percent_change_7d, market_cap, market_cap_dominance, fully_diluted_market_cap, last_updated) VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {}, '{}')
+                    """.format(
+                        data["data"][i]["quote"]["USD"]["price"],
+                        data["data"][i]["quote"]["USD"]["volume_24h"],
+                        data["data"][i]["quote"]["USD"]["volume_change_24h"],
+                        data["data"][i]["quote"]["USD"]["percent_change_1h"],
+                        data["data"][i]["quote"]["USD"]["percent_change_24h"],
+                        data["data"][i]["quote"]["USD"]["percent_change_7d"],
+                        data["data"][i]["quote"]["USD"]["market_cap"],
+                        data["data"][i]["quote"]["USD"]["market_cap_dominance"],
+                        data["data"][i]["quote"]["USD"]["fully_diluted_market_cap"],
+                        data["data"][i]["quote"]["USD"]["last_updated"],
+                    )
+                    self.cursor.execute(requette)
+                self.connection.commit()
+        except Exception as e:
+            self.logger.error(f"Error loading data: {e}")
