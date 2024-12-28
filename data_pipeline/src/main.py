f97e585bc40b4844db2d65071678c6174d7938e5
BouajilaHamza <hamza.bouajila@enetcom.u-sfax.tn>
2024-12-28 12:46:31 +0100

    restructure data_pipeline,remove unused schemas

--- data_pipeline/src/main.py
+++ data_pipeline/src/main.py
@@ -32,16 +32,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="src/static"), name="static")
 templates = Jinja2Templates(directory="./src/templates")
-
-
-class LoadRequest(BaseModel):
-    loader_type: str
-    data_type: str
 
 
 @app.get("/", response_class=HTMLResponse)
