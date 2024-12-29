81af3e6e6bca79e44b490fb41d562060b5229024
BouajilaHamza <hamza.bouajila@enetcom.u-sfax.tn>
2024-12-29 22:30:27 +0100

    update the admin page add redpanda ocnsole and airflow dashbord on the admin page

--- data_pipeline/src/main.py
+++ data_pipeline/src/main.py
@@ -1,33 +1,3 @@
-# from data_pipeline.src.data_ingestion.coin_market_cap_ingestor import (
-#     CoinMarketCapIngestor,
-# )
-# from data_pipeline.src.etl.etl_pipeline import ETLPipeline
-# from data_pipeline.src.etl.redpanda_consumer import RedpandaConsumer
-# from data_pipeline.src.etl.redpanda_producer import RedpandaProducer
-# from data_pipeline.src.storage.snowflake import BitcoinDataLoader
-
-
-# # ,NewsDataLoader
-# def main():
-#     # news_ingestor = NewsDataIngestor()
-#     coin_ingestor = CoinMarketCapIngestor()
-#     producer = RedpandaProducer()
-#     consumer = RedpandaConsumer()
-
-#     bitcoin_loader = BitcoinDataLoader()
-#     # news_loader = NewsDataLoader()
-
-#     bitcoin_pipeline = ETLPipeline([coin_ingestor], producer, consumer, bitcoin_loader)
-#     # news_pipeline = ETLPipeline([news_ingestor], producer,consumer,news_loader)
-
-#     bitcoin_pipeline.run()
-#     # news_pipeline.run()
-
-
-# if __name__ == "__main__":
-#     main()
-
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
