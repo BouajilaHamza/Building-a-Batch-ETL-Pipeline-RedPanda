0a4563f03f055609fa56ea16a553d88b51f977a0
BouajilaHamza <hamza.bouajila@enetcom.u-sfax.tn>
2024-12-29 19:52:56 +0100

    fix dockerfile ,fix envirement variables paths

--- data_pipeline/entrypoint.sh
+++ data_pipeline/entrypoint.sh
@@ -1,6 +1,6 @@
-airflow db init
-airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin --password admin
+poetry run airflow db init
+poetry run airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin --password admin
 
-airflow scheduler &
-airflow webserver --port 5000 &
+poetry run airflow scheduler &
+poetry run airflow webserver --port 5000 &
 poetry run uvicorn src.main:app --host 0.0.0.0 --port 8000
